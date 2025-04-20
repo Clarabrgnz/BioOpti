d66a417a19d29c9a090f79341941c88632a02723
Clarabrgnz <clara.bergonzi@epfl.ch>
2025-04-20 15:44:36 +0200

    Describe your changes briefly

--- src/bioopti/__init__.py
+++ src/bioopti/__init__.py
@@ -1,5 +1,6 @@
 """toolkit for optimizing biochemical processes, including enzyme reaction rates and culture media."""
 
 from __future__ import annotations
+from .sabio import get_kinetic_params_from_sabio
 
 __version__ = "0.0.1"
